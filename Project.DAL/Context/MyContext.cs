9f3cd48d75585b1226f0d20b13c44dd6c69e2925
Doğancan Cedetaş <dogancancedetas@hotmail.com>
2021-10-25 12:20:45 +0300

    DAL katmanı tamamlandı

---
+++ Project.DAL/Context/MyContext.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Project.ENTITIES.Models;
+using Project.MAP.Configurations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.DAL.Context
+{
+
+    //Eğer kurmak istediğiniz veritabanı yapısında Identity kullanacaksanız DbContext'ten miras almazsınız. Çünkü Identity kendi tablolarını tamamen hazır bir yapı olarak sunabilmesi adına sizi IdentityDbContext'tn miras almaya yönlendirir.
+
+    public class MyContext: IdentityDbContext<AppUser, IdentityRole<int>,int>
+    {
+        public MyContext(DbContextOptions<MyContext> options):base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfiguration(new AppUserConfiguration());
+            builder.ApplyConfiguration(new CategoryConfiguration());
+            builder.ApplyConfiguration(new ProductConfiguration());
+            builder.ApplyConfiguration(new ProfileConfiguration());
+            builder.ApplyConfiguration(new OrderConfiguration());
+            builder.ApplyConfiguration(new OrderDetailConfiguration());
+            base.OnModelCreating(builder);
+        }
+
+        public DbSet<AppUser> AppUsers { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<AppUserProfile> Profiles { get; set; }
+        public DbSet<Order> Orders { get; set; }
+    }
+}
---
+++ Project.DAL/Repositories/Concretes/AppUserRepository.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Project.DAL.Context;
+using Project.DAL.Repositories.Abstracts;
+using Project.ENTITIES.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.DAL.Repositories.Concretes
+{
+    public class AppUserRepository: BaseRepository<AppUser>, IAppUserRepository
+    {
+        //Sizin kendinize özel Crud işlemlerinin yine olması gerekir. Ancak unutmayın ki Identity yapısının özel şifrelemeler ve yetkilendirmeleri için hazır async metotları vardır. Bu metotların kullanımı için de ekstra olark bu Repository'de ayrı alanlar açmak en doğrusudur. Bu metotlar Manager sınıfları içerisinde bulundurur.(UserManager, SıgnInManager Identity'de gömülü olan sınıflardır.) Bu sınıflar Dependency Injection ile çalışırlar. ve constructor based injection yapılabilir.
+
+        UserManager<AppUser> _userManager;
+        SignInManager<AppUser> _signInManager;
+
+        public AppUserRepository(MyContext db, UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)  : base(db)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        //Özel bir Identity Metodu async şeklinde tanımlanmalıdır. Çünkü siz burada sonuçta bir API kullanıyorsunuz ve bu API requestlerinin bloklanmadan devam edebilmesi için await keyword'unu kullanmamız gerekir. Bir metot içerisinde await keyword'unu kullanabilmemiz için o metodun async tanımlanması gerekir ve Task döndürmesi gerekir.
+
+        public async Task<bool> AddUser(AppUser item)
+        {
+            //Sadece Asenkron olarak yaratılmış (async marklı) metotlar içerisinde await kullanabilirsiniz
+
+           IdentityResult result = await _userManager.CreateAsync(item, item.PasswordHash);
+            if (result.Succeeded)
+            {
+                await _signInManager.SignInAsync(item, isPersistent: false); //isPersistent durumu Cookie'de dursun mu durmasın mı
+                return true;
+            }
+            return false;
+        }
+    }
+}
